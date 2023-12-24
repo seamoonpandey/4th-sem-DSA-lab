294be33cdb9e0816f0a8cc2ba596c5a3515051a5
Seamoon Pandey <hackedasocial@gmail.com>
2023-12-24 08:59:19 +0545

    add dynamic array stack

--- stack/c/stack.c
+++ stack/c/stack.c
@@ -1,69 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 4
-int stack_arr[MAX];
-int top = -1;
 
-void push(int data)
-{
-	if (top == MAX - 1)
-	{
-		printf("Stack overflow\n");
-		return;
-	}
-	top = top + 1;
-	stack_arr[top] = data;
+#define INITIAL_CAPACITY 10
+ 
+struct Stack{
+	int *arr;
+	int top;
+	int capacity;
+};
+
+
+void initialize(struct Stack *stack){
+	stack -> arr = (int *)malloc(INITIAL_CAPACITY * sizeof(int));
+	stack -> top = -1;
+	stack -> capacity = INITIAL_CAPACITY;
 }
-int pop()
+
+int isEmpty(struct Stack *stack){
+	return stack->top == -1;
+}
+
+void push(struct Stack *stack, int value)
 {
-	if (top == -1)
+	if(stack->top == stack -> capacity - 1){
+		stack->capacity *= 2;
+		stack->arr = (int *)realloc(stack->arr, stack->capacity * sizeof(int));
+	}
+	stack->arr[++stack->top] = value;
+}
+
+int pop(struct Stack *stack)
+{
+	if(isEmpty(stack))
 	{
 		printf("Stack underflow\n");
-		exit(1); // abnormal termination of program
+		exit(1);
 	}
-	int value;
-	value = stack_arr[top];
-	top = top - 1;
-	return value;
-}
-int topElement()
-{
-	if (top == -1)
-	{
-		printf("Empty stack");
-		return 0;
-	}
-	printf("%d\n", stack_arr[top]);
-	return stack_arr[top];
-}
-void print()
-{
-	if (top == -1)
-	{
-		printf("Stack underflow\n");
-		return;
-	}
-	for (int i = top; i >= 0; i--)
-		printf("%d ", stack_arr[i]);
-
-	printf("\n");
+	return stack->arr[stack->top--];
 }
 
-int main()
-{
-	int data;
-	push(1);
-	topElement();
-	push(2);
-	topElement();
-	push(3);
-	topElement();
-	print();
-	push(4);
-	print();
-	pop();
-	print();
-	pop();
-	print();
-	return 0;
+int peek(struct Stack *stack){
+	if(isEmpty(stack)){
+		printf("Stack is empty\n");
+		exit(1);
+	}
+	return stack -> arr[stack->top];
+}
+
+void destroy(struct Stack *stack){
+	free(stack->arr);
+}
+
+int main() {
+    struct Stack myStack;
+    initialize(&myStack);
+
+    push(&myStack, 10);
+    push(&myStack, 20);
+    push(&myStack, 30);
+	pop(&myStack);
+
+    printf("Top element: %d\n", peek(&myStack));
+
+    printf("Popped element: %d\n", pop(&myStack));
+    printf("Popped element: %d\n", pop(&myStack));
+
+    printf("Is the stack empty? %s\n", isEmpty(&myStack) ? "Yes" : "No");
+
+    destroy(&myStack);
+
+    return 0;
 }
