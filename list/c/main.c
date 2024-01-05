8bc44d545df6e39c4736c43ec415fa2a1546b1f6
Seamoon Pandey <hackedasocial@gmail.com>
2024-01-05 09:09:18 +0545

    add reverse operation

--- list/c/main.c
+++ list/c/main.c
@@ -5,61 +5,24 @@ int main()
 {
     struct LinkedList list = {.head = NULL};
 
-    int choice;
-    int data, index;
+    Linkedlist list = {.head = NULL};
 
-    do
-    {
-        printf("\n1. Insert at the end\n");
-        printf("2. Insert at the start\n");
-        printf("3. Insert at a specific index\n");
-        printf("4. Delete at a specific index\n");
-        printf("5. Print the list\n");
-        printf("0. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+    insertNodeAtEnd(&list, 1);
+    insertNodeAtEnd(&list, 2);
+    insertNodeAtEnd(&list, 3);
+    insertNodeAtEnd(&list, 100);
 
-        switch (choice)
-        {
-        case 1:
-            printf("Enter data to insert at the end: ");
-            scanf("%d", &data);
-            insertNodeAtEnd(&list, data);
-            break;
+    printList(&list);
+    insertNodeAtStart(&list, 22);
+    printList(&list);
+    insertNodeAtThatIndex(&list, 2, 7);
+    printList(&list);
 
-        case 2:
-            printf("Enter data to insert at the start: ");
-            scanf("%d", &data);
-            insertNodeAtStart(&list, data);
-            break;
+    deleteNode(&list, 3);
+    printList(&list);
 
-        case 3:
-            printf("Enter index to insert at: ");
-            scanf("%d", &index);
-            printf("Enter data to insert at index %d: ", index);
-            scanf("%d", &data);
-            insertNodeAtThatIndex(&list, index, data);
-            break;
-
-        case 4:
-            printf("Enter index to delete: ");
-            scanf("%d", &index);
-            deleteNode(&list, index);
-            break;
-
-        case 5:
-            printList(&list);
-            break;
-
-        case 0:
-            printf("Exiting the program.\n");
-            break;
-
-        default:
-            printf("Invalid choice. Please try again.\n");
-            break;
-        }
-    } while (choice != 0);
+    reverseList(&list);
+    printList(&list);
 
     return 0;
 }
