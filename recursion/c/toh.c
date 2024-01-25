958459d540a21e17c1896c6581d86a310811cbab
Seamoon Pandey <hackedasocial@gmail.com>
2024-01-25 08:42:32 +0545

    add toh code with recursion

---
+++ recursion/c/toh.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void tower(int  n, char begin, char end, char aux)
+{
+	if(n==0)
+	{
+	//	printf("Move disk %d from rod %c to rod %c\n",n,begin,end);
+		return;
+	}
+	tower(n-1, begin, aux, end);
+	printf("Move disk %d from rod %c to rod %c\n",n,begin,end);
+	tower(n-1,  aux, end,begin);
+}
+
+int main()
+{
+	int n = 3;
+	tower(n, 'x','y','z');
+	return 0;
+}
