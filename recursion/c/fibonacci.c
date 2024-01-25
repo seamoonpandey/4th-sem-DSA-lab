5c8d7c88dfb8382421cebbfd9ad985f54fbb4e14
Seamoon Pandey <hackedasocial@gmail.com>
2024-01-25 07:30:46 +0545

    term starting from 1

--- recursion/c/fibonacci.c
+++ recursion/c/fibonacci.c
@@ -1,19 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int fibonacci(int n)
 {
-	if(n<2){
-		return n;
-	}
+    if (n < 0)
+    {
+        fprintf(stderr, "cannot find fibonacci term for negative index");
+        exit(1);
+    }
+    else if (n < 2)
+    {
+        return 1;
+    }
 
-	return fibonacci(n-1) + fibonacci(n-2);
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main()
 {
-	int n = 10;
+    int n;
 
-	for(int i = 0 ; i<n; i++)
-		printf("%d\t",fibonacci(i));
-	return 0;
+    puts("Enter the term to which you need fibonacci series");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        printf("%d\t", fibonacci(i));
+    puts("");
+    return 0;
 }
