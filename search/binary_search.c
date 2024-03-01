b216129621a4f199f5c138f8c3266d3444b1bcc4
Seamoon Pandey <hackedasocial@gmail.com>
2024-03-01 10:08:15 +0545

    fix search code

--- search/binary_search.c
+++ search/binary_search.c
@@ -1,34 +1,44 @@
-#include<stdio.h>
+#include <stdio.h>
 
-// this program is for the binary search algorithm
-// namely divide and conquer
+// Binary search function
+int binarySearch(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
 
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
 
-int search(int arr[],int size, int target)
-{
-	int right = 0;
-	int left = size-1;
+        // Check if target is present at mid
+        if (arr[mid] == target) {
+            return mid;
+        }
 
-	while(left <= right)
-	{
-		int mid = left + (right-left)/2;
-		if (arr[mid] == target) return mid;
-		
-		if (arr[mid] < target)
-			left = mid + 1;
-		else
-			right = mid - 1;
-	}
+        // If target greater, ignore left half
+        if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        // If target is smaller, ignore right half
+        else {
+            right = mid - 1;
+        }
+    }
 
-	return -1;
+    // Target not found
+    return -1;
 }
 
-
-int main()
-{
-	int arr[] = {1,2,3,4,5};
-	int size = sizeof(arr)/sizeof(int);
-	int index = search(arr, size, 3);	
-	printf("%d",index);
-	return 0;
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+    int index = binarySearch(arr, size, target);
+    
+    if (index != -1) {
+        printf("Element found at index %d", index);
+    } else {
+        printf("Element not found");
+    }
+    
+    return 0;
 }
+
